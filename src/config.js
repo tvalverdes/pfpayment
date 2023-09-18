c91c3e58c1b2cc072e8a9d3b93ecd4a6eedb9a31
Tyrone <tvalverdes9@gmail.com>
2023-09-18 01:40:30 -0500

    new commit

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from 'dotenv'
 import mongoose from 'mongoose'
 config()
 
-export const PORT = process.env.PORT
+export const PORT = process.env.PORT || 3000
 export const HOST = process.env.HOST
 export const KEY = process.env.KEY
 const MONGODB_URI = process.env.MONGODB_URI
